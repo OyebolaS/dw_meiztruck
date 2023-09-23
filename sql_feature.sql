bb1d587771272ee61c0d061dd8a98c57afb1217d
OyebolaS <oyebolafakayejo@gmail.com>
2023-09-23 03:57:25 +0100

    this creates customers table

--- sql_feature.sql
+++ sql_feature.sql
@@ -4,22 +4,3 @@ CREATE TABLE "customers"(
     "email" VARCHAR(255) NOT NULL,
     "reg_date" DATE NOT NULL
 );
-
-ALTER TABLE
-    "customers" ADD PRIMARY KEY("id");
-
-    CREATE TABLE "items"(
-    "id" BIGINT NOT NULL,
-    "item_name" VARCHAR(255) NOT NULL,
-    "cost_price" DOUBLE PRECISION NOT NULL,
-    "selling_price" DOUBLE PRECISION NOT NULL
-
-
-ALTER TABLE
-    "customers" ADD PRIMARY KEY("id");
-CREATE TABLE "items"(
-    "id" BIGINT NOT NULL,
-    "item_name" VARCHAR(255) NOT NULL,
-    "cost_price" DOUBLE PRECISION NOT NULL,
-    "selling_price" DOUBLE PRECISION NOT NULL
-);
